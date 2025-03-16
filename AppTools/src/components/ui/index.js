b36d8e4f1dc648ad5bea2b2424240966c3a0b6e3
chris <5sensprod@gmail.com>
2025-03-16 02:39:18 +0100

    Nettoyage

--- AppTools/src/components/ui/index.js
+++ AppTools/src/components/ui/index.js
@@ -1,6 +1,4 @@
 // src/components/ui/index.js
 export { default as TabNavigation } from './TabNavigation';
-export { default as StatusBadge } from './StatusBadge';
-export { default as DataField } from './DataField';
 export { default as ActionButton } from './ActionButton';
 export { default as InfoCard } from './InfoCard';
