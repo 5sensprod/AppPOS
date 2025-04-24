7a7c9461fad7a50dead1a500c033205b412c8718
chris <5sensprod@gmail.com>
2025-04-24 16:27:06 +0200

    clean

--- AppTools/preload.js
+++ AppTools/preload.js
@@ -3,25 +3,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // Configuration standard des APIs exposées à la fenêtre
 contextBridge.exposeInMainWorld('electronAPI', {
-  // Vos fonctions API existantes...
   openWebCaptureWindow: (url, options) => ipcRenderer.send('open-web-capture-window', url, options),
 
-  // NOUVELLES FONCTIONS POUR LA GESTION DES PRODUITS CAPTURÉS
-
-  // Envoyer des données de produit capturées au processus principal
-  sendCapturedProductData: (data) => ipcRenderer.send('captured-product-data', data),
-
   onUpdateMessage: (callback) => ipcRenderer.on('update-message', (_, data) => callback(data)),
-
-  // Écouter les mises à jour de produits capturés
-  onCapturedProductUpdate: (callback) =>
-    ipcRenderer.on('captured-product-update', (_, data) => callback(data)),
-
-  // Demander l'état actuel des produits capturés
-  requestCapturedProductsState: () => ipcRenderer.send('request-captured-products-state'),
-
-  // Exporter les produits capturés
-  exportCapturedProducts: (products) => ipcRenderer.send('export-captured-products', products),
 });
 
 // Configurer la communication entre la WebView et l'application principale
